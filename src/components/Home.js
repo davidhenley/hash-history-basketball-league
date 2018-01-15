7deede00eba494c149a62b8a8c5fb64bd051c406
David Henley <henleydr@gmail.com>
2018-01-15 11:00:34 -0600

    Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import TeamLogo from './TeamLogo';
+import { getTeamNames } from '../api';
 
 class Home extends Component {
+  state = {
+    teamNames: []
+  };
+  async componentDidMount() {
+    let teamNames = await getTeamNames();
+    this.setState({ teamNames });
+  }
   render() {
-    return <div className="container">Home</div>;
+    const { teamNames } = this.state;
+    return (
+      <div className="container">
+        <h1 className="large-header">Hash History Basketball League</h1>
+        <h3 className="header text-center">Select a team</h3>
+
+        <div className="home-grid">
+          {teamNames.map(teamName => (
+            <Link key={teamName} to={`/${teamName}`}>
+              <TeamLogo id={teamName} width="125px" />
+            </Link>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
