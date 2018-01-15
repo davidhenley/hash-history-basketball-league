ef1b6414fd0f8bbbfe5482feb4beef6b33df46e6
David Henley <henleydr@gmail.com>
2018-01-15 10:38:39 -0600

    Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './Home';
+import Navbar from './Navbar';
 import Players from './Players';
 import Teams from './Teams';
 
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <Router>
       <div>
+        <Navbar />
         <Route exact path="/" component={Home} />
         <Route path="/players" component={Players} />
         <Route path="/teams" component={Teams} />
