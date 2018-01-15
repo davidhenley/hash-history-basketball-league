f3194faa7598efa831fba30d7d79ea14a05cf0dd
David Henley <henleydr@gmail.com>
2018-01-15 10:42:06 -0600

    NoMatch

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
 import Navbar from './Navbar';
@@ -11,9 +11,12 @@ const App = () => {
     <Router>
       <div>
         <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route path="/players" component={Players} />
-        <Route path="/teams" component={Teams} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/players" component={Players} />
+          <Route path="/teams" component={Teams} />
+          <Route render={() => <h1 className="text-center">Four oh Four.</h1>} />
+        </Switch>
       </div>
     </Router>
   );
